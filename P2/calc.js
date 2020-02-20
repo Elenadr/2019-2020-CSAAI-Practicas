c2a58277fd96aa0e35f91905218bb474ec21d777
Elena Del Rio <elenadr@MacBook-Air-de-Elena.local>
2020-02-20 20:41:11 +0100

    probando un boton

--- P2/calc.js
+++ P2/calc.js
@@ -1,1 +1,11 @@
 console.log("Launching Javascript... ");
+
+const hola = document.getElementById('hola')
+hola.onclick = () =>{
+  console.log("Click!!");
+  if (  hola.style.backgroundColor==''){
+    hola.style.backgroundColor="lightpink";
+  }else{
+    hola.style.backgroundColor='';
+  }
+}
