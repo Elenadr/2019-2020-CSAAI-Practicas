ac8b2fe6ef51178c1f2ddb98731644247b5fa345
Elena Maria  Del Rio Galera <elenadr@f-l3210-pc05.aulas.etsit.urjc.es>
2020-02-27 10:41:43 +0100

    changes

--- P2/calc.js
+++ P2/calc.js
@@ -1,12 +1,48 @@
+//Calculadora funcional
+
 console.log("Launching Javascript... ");
 
-const hola = document.getElementById('hola')
-boton1.onclick = () =>{
-  console.log("Click!!");
-  if (  boton1.style.backgroundColor==''){
-    boton1.style.backgroundColor="lightpink";
-  }else{
-    boton1.style.backgroundColor='';
+const gui ={
+  display:document.getElementById("display"),
+  delete:document.getElementById("delete"),
+  clear:document.getElementById("clear"),
+  multiplication:document.getElementById("multiplication"),
+  division:document.getElementById("division"),
+  sum:document.getElementById("sum"),
+  subtraction:document.getElementById("subtraction"),
+  dot:document.getElementById("dot"),
+  exp:document.getElementById("exp"),
+  ans:document.getElementById("ans"),
+  equal:document.getElementById("equal"),
+}
+let digitos = document.getElementsByClassName("cdigito");
+for (i=0; i<digitos.length; i++){
+  digitos[i].onclick = (ev)=>{
+    digito(ev.target);
   }
-  console.log(3);
+}
+function digito(boton){
+  if(display.innerHTML=="0"){
+    display.innerHTML = boton.value;
+  }else{
+    display.innerHTML += boton.value;
+  }
+}
+
+//-- Insertar simbolo de sumar
+sum.onclick = () => {
+  display.innerHTML += sum.value;
+    console.log("click en el +");
+}
+
+//-- Evaluar la expresion
+equal.onclick = () => {
+  display.innerHTML = eval(display.innerHTML);
+    console.log("click en el =");
+}
+
+//-- Poner a cero la expresion
+clear.onclick = () => {
+  display.innerHTML = "0";
+    console.log("clear");
 }
