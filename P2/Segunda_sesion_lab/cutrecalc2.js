e12c5c7c9c9fbc9c8d79040dbd3aa5ea1e6f3719
Elena Maria  Del Rio Galera <elenadr@f-l3210-pc05.aulas.etsit.urjc.es>
2020-02-27 10:11:04 +0100

    improving code

---
+++ P2/Segunda_sesion_lab/cutrecalc2.js
@@ -0,0 +1,44 @@
+//-- Contador de clicks de boton
+
+console.log("Ejecutando JS...");
+
+//-- Acceder a los elementos del DOM
+const gui ={
+  display:document.getElementById("display"),
+  suma:document.getElementById("suma"),
+  igual:document.getElementById("igual"),
+  clear:document.getElementById("clear"),
+}
+let digitos = document.getElementsByClassName("cdigito");
+
+for (i=0; i<digitos.length; i++){
+  digitos[i].onclick = (ev)=>{
+    digito(ev.target);
+  }
+}
+
+function digito(boton){
+  if(display.innerHTML=="0"){
+    display.innerHTML = boton.value;
+  }else{
+    display.innerHTML += boton.value;
+  }
+}
+
+//-- Insertar simbolo de sumar
+suma.onclick = () => {
+  display.innerHTML += suma.value;
+    console.log("click en el +");
+}
+
+//-- Evaluar la expresion
+igual.onclick = () => {
+  display.innerHTML = eval(display.innerHTML);
+    console.log("click en el =");
+}
+
+//-- Poner a cero la expresion
+clear.onclick = () => {
+  display.innerHTML = "0";
+    console.log("clear");
+}
