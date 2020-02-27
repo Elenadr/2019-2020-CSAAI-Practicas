aedfad5afd615a1f2d4862e5dabea9c5243d0265
Elena Maria  Del Rio Galera <elenadr@f-l3210-pc05.aulas.etsit.urjc.es>
2020-02-27 09:39:57 +0100

    looking up like a calculator

--- P2/Segunda_sesion_lab/cutrecalc.js
+++ P2/Segunda_sesion_lab/cutrecalc.js
@@ -6,32 +6,38 @@ console.log("Ejecutando JS...");
 const gui ={
   display:document.getElementById("display"),
   boton1: document.getElementById("boton1"),
-  boton2: document.getElementById("boton2")
+  boton2: document.getElementById("boton2"),
+  suma:document.getElementById("suma"),
+  igual:document.getElementById("igual"),
+  clear:document.getElementById("clear"),
 }
 
-//-- Contador de clicks
-const counter = {
-  valor: 0,
-  inc : function(value) {
-    this.valor += value;
-    gui.display.innerHTML = this.valor;
-  }
+// -- Insertar digito 1
+boton1.onclick = () => {
+  display.innerHTML += "1";
+  console.log("click en el 1");
 }
 
-
-//Incremento automatico contador cada 1 sg
-//1000ms
-setInterval(()=>{
-  console.log("tic-tac");
-  counter.inc(1);
-},1000);
-//suma
-gui.boton1.onclick = () => {
-  console.log("Click!");
-  counter.inc(1);
+//-- Insertar digito 2
+boton2.onclick = () => {
+  display.innerHTML += "2";
+    console.log("click en el 2");
 }
-  //resta
-  gui.boton2.onclick = () => {
-    console.log("Click!");
-    counter.inc(-1);
-  }
+
+//-- Insertar simbolo de sumar
+suma.onclick = () => {
+  display.innerHTML += "+";
+    console.log("click en el +");
+}
+
+//-- Evaluar la expresion
+igual.onclick = () => {
+  display.innerHTML = eval(display.innerHTML);
+    console.log("click en el =");
+}
+
+//-- Poner a cero la expresion
+clear.onclick = () => {
+  display.innerHTML = "0";
+    console.log("clear");
+}
