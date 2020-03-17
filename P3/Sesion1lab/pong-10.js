8a41ac5e1cf8eb5c324bf9a4b75393e3d7c696ff
Elena Del Rio <elenadr@MacBook-Air-de-Elena.local>
2020-03-17 13:36:21 +0100

    todo mal

--- P3/Sesion1lab/pong-10.js
+++ P3/Sesion1lab/pong-10.js
@@ -5,16 +5,16 @@ const canvas = document.getElementById("canvas");
 
 //-- Sus dimensiones las hemos fijado en el fichero
 //-- HTML. Las imprimimos en la consola
-
 console.log(`canvas: Anchura: ${canvas.width}, Altura: ${canvas.height}`);
 
 //-- Obtener el contexto para pintar en el canvas
 const ctx = canvas.getContext("2d");
-//-- Pintar todos los objetos en el canvas
+
 //-- Variables para la bola
 let bola_x = 50;
 let bola_vx = 0;
 
+//-- Pintar todos los objetos en el canvas
 function draw() {
 
   //----- Dibujar la Bola
@@ -60,56 +60,48 @@ function draw() {
   ctx.fillText("0", 200, 80);
   ctx.fillText("1", 340, 80);
 }
-//---- Bucle principal de la animación
 
+//---- Bucle principal de la animación
 function animacion()
 {
 
+  //-- Actualizar las posiciones de los objetos móviles
 
-    //-- Actualizar las posiciones de los objetos móviles
+  //-- Comprobar si la bola ha alcanzado el límite derecho
+  //-- Si es así, se cambia de signo la velocidad, para
+  // que "rebote" y vaya en el sentido opuesto
+  if (bola_x >= canvas.width) {
+    //-- Hay colisión. Cambiar el signo de la bola
+    bola_vx = bola_vx * -1;
+  }
+  if (bola_x <= canvas.width) {
+    bola_vx = bola_vx * +1;
+  }
 
-    //-- Comprobar si la bola ha alcanzado el límite derecho
-    //-- Si es así, se cambia de signo la velocidad, para
-    // que "rebote" y vaya en el sentido opuesto
-    if (bola_x >= canvas.width) {
-      //-- Hay colisión. Cambiar el signo de la bola
-      bola_vx = bola_vx * -1;
-    }
+  //-- Actualizar coordenada x de la bola, en funcion de
+  //-- su velocidad
+  bola_x += bola_vx;
 
-    //-- Actualizar coordenada x de la bola, en funcion de
-    //-- su velocidad
-    bola_x += bola_vx;
+  //-- Borrar la pantalla
+  ctx.clearRect(0,0, canvas.width, canvas.height);
 
-    //-- Borrar la pantalla
-    ctx.clearRect(0,0, canvas.width, canvas.height);
-
-    //-- Dibujar el nuevo frame
-    draw();
+  //-- Dibujar el nuevo frame
+  draw();
 }
+
 //-- Arrancar la animación
 setInterval(()=>{
   animacion();
 },16);
 
-//-- Obtener el boton de dar un "paso"
-const reset = document.getElementById("reset");
-
-//-- Botón de dar un Paso: Cada vez que lo apretamos
-//-- la bola avanza 5 píxeles
-reset.onclick = () => {
-  //-- Incrementar la posicion x de la bola
-  bola_x = 50;
-  console.log("Reset!");
-}
-//-- Obtener el boton de dar un "paso"
+//-- Obtener el boton de saque
 const sacar = document.getElementById("sacar");
 
-//-- Botón de dar un Paso: Cada vez que lo apretamos
-//-- la bola avanza 5 píxeles
+//-- Botón de saque:
+//-- Dar a la bola una velocidad inicial
+//-- También restablecemos la posicion inicial
 sacar.onclick = () => {
-  //-- Incrementar la posicion x de la bola
   bola_x = 50;
-  bola_vx=6;
-  console.log("sacar");
-
+  bola_vx = 6;
+  console.log("Saque!");
 }
