271392273f9785d61d396e26ab919e81d33c4b65
Elena Del Rio <elenadr@MacBook-Air-de-Elena.local>
2020-03-23 17:25:01 +0100

    suenaaaa

---
+++ P3/Sesion3lab/pong-18.js
@@ -0,0 +1,151 @@
+console.log("Ejecutando JS...");
+
+//-- Obtener el objeto canvas
+const canvas = document.getElementById("canvas");
+
+//-- Sus dimensiones las hemos fijado en el fichero
+//-- HTML. Las imprimimos en la consola
+console.log(`canvas: Anchura: ${canvas.width}, Altura: ${canvas.height}`);
+
+//-- Obtener el contexto para pintar en el canvas
+const ctx = canvas.getContext("2d");
+
+//-- Obtener Sonidos
+const sonido_raqueta = new Audio("pong-raqueta.mp3");
+const sonido_rebote = new Audio("pong-rebote.mp3");
+
+//-- Pintar todos los objetos en el canvas
+function draw() {
+
+  //----- Dibujar la Bola
+  bola.draw();
+
+  //-- Dibujar las raquetas
+  raqI.draw();
+  raqD.draw();
+
+  //--------- Dibujar la red
+  ctx.beginPath();
+
+  //-- Estilo de la linea: discontinua
+  //-- Trazos de 10 pixeles, y 10 de separacion
+  ctx.setLineDash([10, 10]);
+  ctx.strokeStyle = 'white';
+  ctx.lineWidth = 2;
+  //-- Punto superior de la linea. Su coordenada x está en la mitad
+  //-- del canvas
+  ctx.moveTo(canvas.width/2, 0);
+
+  //-- Dibujar hasta el punto inferior
+  ctx.lineTo(canvas.width/2, canvas.height);
+  ctx.stroke();
+
+  //------ Dibujar el tanteo
+  ctx.font = "100px Arial";
+  ctx.fillStyle = "white";
+  ctx.fillText("0", 200, 80);
+  ctx.fillText("1", 340, 80);
+}
+
+//---- Bucle principal de la animación
+function animacion()
+{
+
+  //-- Actualizar las posiciones de los objetos móviles
+
+  //-- Actualizar la raqueta con la velocidad actual
+  raqI.update();
+  raqD.update();
+
+
+  //-- Comprobar si la bola ha alcanzado el límite derecho
+  //-- Si es así, se cambia de signo la velocidad, para
+  // que "rebote" y vaya en el sentido opuesto
+  if (bola.x >= canvas.width) {
+    //-- Hay colisión. Cambiar el signo de la bola
+    bola.vx = bola.vx * -1;
+    //-- Reproducir sonido
+    sonido_rebote.currentTime = 0;
+    sonido_rebote.play();
+  }
+
+  //-- Comprobar si hay colisión con la raqueta izquierda
+  if (bola.x >= raqI.x && bola.x <=(raqI.x + raqI.width) &&
+      bola.y >= raqI.y && bola.y <=(raqI.y + raqI.height)) {
+    bola.vx = bola.vx * -1;
+
+    //-- Reproducir sonido
+    sonido_raqueta.currentTime = 0;
+    sonido_raqueta.play();
+  }
+
+  //-- Actualizar coordenada x de la bola, en funcion de
+  //-- su velocidad
+  bola.update()
+
+  //-- Borrar la pantalla
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+
+  //-- Dibujar el nuevo frame
+  draw();
+}
+
+//-- Inicializa la bola: Llevarla a su posicion inicial
+const bola = new Bola(ctx);
+
+//-- Crear las raquetas
+const raqI = new Raqueta(ctx);
+const raqD = new Raqueta(ctx);
+
+//-- Cambiar las coordenadas de la raqueta derecha
+raqD.x_ini = 540;
+raqD.y_ini = 300;
+raqD.init();
+
+//-- Arrancar la animación
+setInterval(()=>{
+  animacion();
+},16);
+
+//-- Retrollamada de las teclas
+window.onkeydown = (e) => {
+
+  switch (e.key) {
+    case "a":
+      raqI.v = raqI.v_ini;
+      break;
+    case "q":
+      raqI.v = raqI.v_ini * -1;
+      break;
+    case "p":
+      raqD.v = raqD.v_ini * -1;
+      break;
+    case "l":
+      raqD.v = raqD.v_ini;
+      break;
+    case " ":
+
+      //-- Reproducir sonido
+      sonido_raqueta.currentTime = 0;
+      sonido_raqueta.play();
+
+      //-- Llevar bola a su posicion incicial
+      bola.init();
+
+      //-- Darle velocidad
+      bola.vx = bola.vx_ini;
+    default:
+  }
+}
+
+//-- Retrollamada de la liberacion de teclas
+window.onkeyup = (e) => {
+  if (e.key == "a" || e.key == "q"){
+    //-- Quitar velocidad de la raqueta
+    raqI.v = 0;
+  }
+
+  if (e.key == "p" || e.key == "l") {
+    raqD.v = 0;
+  }
+}
